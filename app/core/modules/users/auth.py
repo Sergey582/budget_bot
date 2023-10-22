68ad38e67602506068291ec722b5fd149d1646b3
sergey582 <ser.matyushenok@gmail.com>
2023-10-22 16:56:58 +0200

    change auth logic

--- app/core/modules/users/auth.py
+++ app/core/modules/users/auth.py
@@ -1,5 +1,6 @@
 import hashlib
 import hmac
+from urllib.parse import parse_qs
 
 from app.core.config import BOT_TOKEN
 from app.core.modules.spending.models import User
@@ -22,8 +23,29 @@ class AuthenticationFailed(HTTPException):
         super().__init__(status_code=status_code, detail=detail)
 
 
-def verify_telegram_data(data_check_string: str, received_hash: str) -> bool:
-    secret_key = hashlib.sha256(BOT_TOKEN.encode('utf-8'))
+def calculate_hmac_sha256_signature(bot_token):
+    # The secret key is "WebAppData"
+    secret_key = "WebAppData"
+
+    # Convert the secret key and bot token to bytes
+    secret_key_bytes = secret_key.encode('utf-8')
+    bot_token_bytes = bot_token.encode('utf-8')
+
+    # Calculate the HMAC-SHA-256 signature
+    hmac_signature = hmac.new(secret_key_bytes, bot_token_bytes, hashlib.sha256)
+
+    # Get the hexadecimal representation of the signature
+    signature_hex = hmac_signature.hexdigest()
+
+    return hmac_signature
+
+
+def verify_telegram_data(initial_data: str) -> bool:
+    parsed_data = parse_qs(initial_data)
+    received_hash = parsed_data.pop('hash')[0]
+    data_check_string = '\n'.join([f"{key}={parsed_data[key][0]}" for key in sorted(parsed_data.keys())])
+
+    secret_key = calculate_hmac_sha256_signature(BOT_TOKEN)
     message = data_check_string.encode('utf-8')
     generated_hash = hmac.new(secret_key.digest(), message, hashlib.sha256).hexdigest()
     return generated_hash == received_hash
@@ -33,10 +55,7 @@ async def user_auth_check(request: Request, credentials: Credentials = Depends()
     if not credentials:
         raise AuthenticationFailed
 
-    if not verify_telegram_data(
-            data_check_string=credentials.data_check_string,
-            received_hash=credentials.received_hash,
-    ):
+    if not verify_telegram_data(initial_data=credentials.data_check_string):
         raise AuthenticationFailed("invalid credentials")
 
     user_id = credentials.telegram_user_id
